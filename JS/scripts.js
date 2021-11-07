aa7fb4bdf06bb83f6e9ba582f8ab9cfb944f8c50
Timelord514 <92347894+Timelord514@users.noreply.github.com>
2021-11-07 00:31:26 -0700

    Add radio selectors, form-control, and input.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("form#language-selector").submit(function(event) {
     event.preventDefault();
-    
+
+    const color = $("#question1").val();
+    const shrimp = $("#question2").val();
+    const moon = $("#question3").val();
+    const games = $("#question4").val();
+    const choice = $("#question5").val();
+    const theAnswer = $("#question6").val;
+
+
+
   })
 })
