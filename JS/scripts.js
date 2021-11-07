315a19be859cc0f99808956519925b51ed514346
Timelord514 <92347894+Timelord514@users.noreply.github.com>
2021-11-07 01:12:52 -0800

    Add clean-ip to html, css, and js.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-  $("form#language-selector").submit(function(event) {
+$(document).ready(function () {
+  $("form#language-selector").submit(function (event) {
     event.preventDefault();
 
-    const color = $("#question1").val();
-    const shrimp = $("#question2").val();
-    const moon = $("#question3").val();
-    const games = $("#question4").val();
-    const choice = $("#question5").val();
-    const theAnswer = $("#question6").val;
+    const color = parseInt($("#question1").val());
+    const shrimp = parseInt($("#question2").val());
+    const moon = parseInt($("#question3").val());
+    const games = parseInt($("#question4").val());
+    const choice = parseInt($("#question5").val());
+    const theAnswer = parseInt($("#question6").val());
+
+    if (total < 18) {
+      let resultVal = "Duplo";
+      $("#programerLang").text(resultVal);
+    } else if (total >= 18 && 88) {
+      let resultVal = "C#";
+      $("#programerLang").text(resultVal);
+    } else if (total > 88 && 182) {
+      let resultVal = "Rust";
+      $("#programerLang").text(resultVal);
+    } else if (total > 182 && 275) {
+      let resultVal = "Python";
+      $("#programerLang").text(resultVal);
+    } else {
+      let resultVal = "Ruby";
+      $("#programerLang").text(resultVal);
+    }
+    $("#result").show();
 
 
-
-  })
-})
+  });
+});
